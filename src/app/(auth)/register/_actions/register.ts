8ee911b068870bd9455c837532e72b4f7141c1a3
Jessica Bandeira <jessica.santosb@outlook.com>
2024-09-13 13:56:09 -0300

    feat: handle register flow

--- src/app/(auth)/register/_actions/register.ts
+++ src/app/(auth)/register/_actions/register.ts
@@ -1,5 +1,24 @@
-'use server'
+'use server';
 
-export default async function register() {
-  return console.log('registramos');
+import db from '@/lib/db';
+import { hashSync } from 'bcrypt-ts';
+
+export default async function register(FormData: FormData) {
+  const name = FormData.get('name') as string;
+  const email = FormData.get('email') as string;
+  const password = FormData.get('password') as string;
+
+  if (!name || !email || !password) {
+    throw new Error('Todos os campos devem ser preenchidos!');
+  }
+
+  const user = await db.user.findUnique({
+    where: { email: email },
+  });
+
+  if (user) throw new Error('Esse usuário já existe!');
+
+  await db.user.create({
+    data: { name, email, password: hashSync(password) },
+  });
 }
