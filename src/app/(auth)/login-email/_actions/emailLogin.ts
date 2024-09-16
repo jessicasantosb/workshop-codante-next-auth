48a336025cbc6304b4ea030667b34e599196eb63
Jessica Bandeira <jessica.santosb@outlook.com>
2024-09-16 15:19:44 -0300

    feat: setup nodemailer

--- src/app/(auth)/login-email/_actions/emailLogin.ts
+++ src/app/(auth)/login-email/_actions/emailLogin.ts
@@ -1,4 +1,8 @@
 'use server';
 
-export default async function emailLogin() {
+import { signIn } from '@/auth';
+
+export default async function emailLogin(FormData: FormData) {
+  const email = FormData.get('email') as string;
+  await signIn('nodemailer', { email });
 }
--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,7 @@ import { compareSync } from 'bcrypt-ts';
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
+import EmailProvider from 'next-auth/providers/nodemailer';
 import db from './lib/db';
 
 const prisma = new PrismaClient();
@@ -47,6 +48,17 @@ export const {
     }),
     GithubProvider({
       allowDangerousEmailAccountLinking: true,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
     }),
   ],
 });
